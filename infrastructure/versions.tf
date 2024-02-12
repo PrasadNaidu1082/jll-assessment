01cfbfe728d3d20f750c454873fa938fb47d5293
Prasad Naidu <prasadnaidu1082@outlook.com>
2024-02-12 08:23:07 +0530

    removed agro config

--- infrastructure/versions.tf
+++ infrastructure/versions.tf
@@ -8,3 +8,20 @@ terraform {
 
   required_version = ">= 0.14"
 }
+
+provider "helm" {
+  kubernetes {
+    config_path = "~/.kube/config"
+  }
+}
+
+terraform {
+  required_version = ">= 1.0.0"
+
+  required_providers {
+    helm = {
+      source  = "hashicorp/helm"
+      version = "= 2.5.1"
+    }
+  }
+}
